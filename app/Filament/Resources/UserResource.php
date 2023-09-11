b142ec75424c0613ae840fb627dfcd1e2330bfbc
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-09-12 06:28:46 +0800

    commented password field in users

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -38,11 +38,11 @@ class UserResource extends Resource
                     ->label("Email")
                     ->email()
                     ->maxLength(255),
-                Forms\Components\DateTimePicker::make("email_verified_at"),
-                Forms\Components\TextInput::make("password")
-                    ->password()
-                    ->required()
-                    ->maxLength(255),
+                // Forms\Components\DateTimePicker::make("email_verified_at"),
+                // Forms\Components\TextInput::make("password")
+                //     ->password()
+                //     ->required()
+                //     ->maxLength(255),
                 Forms\Components\Select::make("roles")->relationship('roles', 'name')->multiple()->preload(),
             ]);
     }
