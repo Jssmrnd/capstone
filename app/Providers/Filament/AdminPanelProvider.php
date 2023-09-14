1395f43a9939a862b14a6478cd385a158e981272
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-09-14 12:53:06 +0800

    added the collapsible

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -41,6 +41,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login(Login::class)
+            ->sidebarCollapsibleOnDesktop()
             ->colors([
                 'primary' => Color::Amber,
             ])
