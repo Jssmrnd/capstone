b701daab9a8c163e58d1127696df4c0863ecacfb
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-09-15 13:58:17 +0800

    added the customer user middleware

--- config/filament-fabricator.php
+++ config/filament-fabricator.php
@@ -27,6 +27,7 @@ return [
         \Illuminate\Cookie\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
+        \App\Http\Middleware\CustomerUser::class,
         // \Illuminate\Session\Middleware\AuthenticateSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
