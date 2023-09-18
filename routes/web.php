b40c6a97b3a646d47f35cbc10f6c803c56d8b9db
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-09-19 06:38:10 +0800

    added home /

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function(){
+    return view('welcome');
+});
+
 Route::get('/login', [CustomerLogin::class, 'index'])
 ->name('login');
 
