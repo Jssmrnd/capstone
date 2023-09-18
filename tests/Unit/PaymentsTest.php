e501b572124a2dc1d97773b54ddb3704ea4519a6
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-09-19 06:38:21 +0800

    tests

---
+++ tests/Unit/PaymentsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class PaymentsTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_negative_amount(): void
+    {
+        $this->assertTrue();
+    }
+
+}
+
+
+
+
