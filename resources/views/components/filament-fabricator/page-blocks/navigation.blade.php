72aa6731ef14a67f3710c448e882f23c32fa64ff
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-09-19 21:30:57 +0800

    customized nav

--- resources/views/components/filament-fabricator/page-blocks/navigation.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/navigation.blade.php
@@ -11,35 +11,59 @@
     @endforeach
 @endif --}}
 
-<header class="text-gray-600 body-font">
-    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-        </svg>
-        <span class="ml-3 text-xl">{{ $company_name }}</span>
-      </a>
-      <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-        <a class="mr-5 hover:text-gray-900" href="/home">Home</a>
+{{-- <header>
+    <a href="../html/index.html" class="logo">BrandName</a>
+        <a class="" href="/home">Home</a>
         <a class="mr-5 hover:text-gray-900" href="/products">Products</a>
-        @auth
-            <a class="mr-5 hover:text-gray-900" href="/application">Application</a>
-        @endauth
-        <a class="mr-5 hover:text-gray-900" href="/contact-us">Contact Us</a>
-        <a class="mr-5 hover:text-gray-900" href="/about-us">About Us</a>
-      </nav>
+            @auth
+                <a class="mr-5 hover:text-gray-900" href="/application">Application</a>
+            @endauth
+            <a class="mr-5 hover:text-gray-900" href="/contact-us">Contact Us</a>
+            <a class="mr-5 hover:text-gray-900" href="/about-us">About Us</a>
         @if (Route::has('login'))
-            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                @auth
-                    <a href= "{{ route('logout') }}" class="mr-5 hover:text-gray-900">Logout</a>
-                @else
-                    <a href="{{ route('login') }}" class="mr-5 hover:text-gray-900">Log in</a>
-
-                    @if (Route::has('register'))
+            @auth
+                <a href= "{{ route('logout') }}" class="mr-5 hover:text-gray-900">Logout</a>
+        @else
+                <a href="{{ route('login') }}" class="mr-5 hover:text-gray-900">Log in</a>
+                @if (Route::has('register'))
                         <a href="{{ route('register') }}" class="mr-5 hover:text-gray-900">Register</a>
-                    @endif
-                @endauth
-            </div>
+                @endif
+            @endauth
         @endif
     </div>
-  </header>
+  </header> --}}
+
+
+  <header>
+    <!-- Brand logo Text -->
+    <a href="#" class="logo">{{ $company_name }}</a>
+    <!-- Navigation bar -->
+    <ul class="navbar">
+        {{-- redirects to the home --}}
+        <li>
+            <a href="/home" class="active">Home</a>
+        </li>
+        {{-- redirects to the products --}}
+        <li>
+            <a href="/products" >Products</a>
+        </li>
+        <li>
+            @auth<a href="/application">Application</a>@endauth
+        </li>
+        {{-- redirects to the about --}}
+        <li>
+            <a href="/about">About</a>
+        </li>
+        {{-- redirects to the contact --}}
+        <li>
+            <a href="/contact-us">Contact Us</a>
+        </li>
+    </ul>
+    <!-- User registration and menu icon -->
+    <div class="main">
+        <a href="/login" class="user" id="loginLink">Log In 
+            <i class="ri-account-circle-fill"></i>
+        </a>
+        <div class="ri-menu-line" id="menu-icon"></div>
+    </div>
+</header>
