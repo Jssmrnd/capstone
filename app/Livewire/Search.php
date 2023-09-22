317ff99b00822e36e01871ffe8475bde208bf633
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-09-22 16:36:59 +0800

    search test

---
+++ app/Livewire/Search.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\UnitModel;
+use Livewire\Component;
+
+class Search extends Component
+{
+    public $search;
+    protected $queryString = ['search'];
+
+    public function render()
+    {
+        return view('livewire.search', [
+            'unit_models' => UnitModel::query()->where('model_name', 'like', "%".$this->search."%")->get(),
+        ]);
+    }
+}
