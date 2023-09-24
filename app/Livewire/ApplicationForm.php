4480bb82ff7cc0b23eff8845c818dea9b8c4a5d9
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-09-24 19:29:43 +0800

    making application form work

--- app/Livewire/ApplicationForm.php
+++ app/Livewire/ApplicationForm.php
@@ -27,106 +27,130 @@ class ApplicationForm extends Component implements HasForms
     public function form(Form $form): Form
     {
         return $form
-                ->schema([
-                    Forms\Components\Fieldset::make("Unit to be Financed")
-                            ->disabledOn('edit')
-                            ->columns(4)
+        ->schema([
+            Forms\Components\Select::make('unit_id')
+                    ->hint("Ex. Mio soul i")
+                    ->columnSpan(2)
+                    ->label('Unit Model')
+                    ->relationship('unitModel', 'model_name')
+                    ->searchable(['unit_model', 'id'])
+                    ->preload()
+                    ->live()
+                    ->afterStateUpdated(
+                            function(Forms\Get $get, Forms\Set $set){
+                                $unit_price = UnitModel::find($get("unit_id"))->price;
+                                $set('unit_srp', $unit_price);
+                            }
+            ),
+
+            Forms\Components\Group::make()
+                    ->columnSpan(4)
+                    ->columns(2)
+                    ->live()
+                    ->disabled(fn (Forms\Get $get): bool => ! $get('unit_id'))
+                    ->schema([
+                            Forms\Components\Select::make('unit_term')
+                                    ->columnSpan(1)
+                                    ->required(true)
+                                    ->label('Term:')
+                                    ->options([
+                                        12 => 12,
+                                        24 => 24,
+                                        36 => 36,
+                                    ]),
+                            Forms\Components\Fieldset::make("Applicant Information")
+                            ->columns(3)
                             ->columnSpan(2)
                             ->schema([
-                                    Forms\Components\Select::make('unit_id')
-                                            ->hint("Ex. Mio soul i")
-                                            ->columnSpan(2)
-                                            ->label('Unit Model')
-                                            ->relationship("unitModel", "model_name")
-                                            ->searchable()
-                                            ->preload()
+                                    Forms\Components\TextInput::make('applicant_surname')
+                                            ->label('First Name:')
+                                            ->columnSpan(1)
+                                            ->required(true),
+                                    Forms\Components\TextInput::make('applicant_middlename')
+                                            ->label('Middle Name:')
+                                            ->columnSpan(1),
+                                    Forms\Components\TextInput::make('applicant_lastname')
+                                            ->label('Last Name:')
+                                            ->columnSpan(1)
+                                            ->required(true),
+                                    Forms\Components\DatePicker::make('applicant_birthday')
+                                            ->label('Birthday:')
+                                            ->maxDate(now()->subYears(150))
+                                            ->maxDate(now())
+                                            ->columnSpan(1)
+                                            ->required(true),
+                                    Forms\Components\TextInput::make('applicant_telephone')
+                                            ->label('Telephone:'),
+                                    Forms\Components\TextInput::make('applicant_valid_id')
+                                            ->label('Valid ID:')
+                                            ->required(true),
+                                    Forms\Components\Select::make('applicant_civil_status')
+                                            ->label('Civil Status')
                                             ->live()
-                                            ->afterStateUpdated(
-                                                    function(Forms\Get $get, Forms\Set $set){
-                                                        $unit_price = UnitModel::find($get("unit_id"))->price;
-                                                        $set('unit_srp', $unit_price);
-                                                    }
-                                    ),
+                                            ->columnSpan(3)
+                                            ->required(true)
+                                            ->options(['single'=> 'Single', 'married' => 'Married', 'separated' => 'Separated', 'widow' => 'Widow']),
+                                    Forms\Components\Textarea::make('applicant_present_address')
+                                            ->label('Present Address:')
+                                            ->columnSpan(1)->required(true),
+                                    Forms\Components\Textarea::make('applicant_previous_address')
+                                            ->label('Previous Address:')
+                                            ->columnSpan(1),
+                                    Forms\Components\Textarea::make('applicant_provincial_address')
+                                            ->label('Provincial Address:')->columnSpan(1),
+                                    Forms\Components\TextInput::make('applicant_lived_there')
+                                            ->numeric()
+                                            ->suffix('year(s)')
+                                            ->inputMode('integer')
+                                            ->label('Lived There:'),
+                                    Forms\Components\Select::make('applicant_house')
+                                            ->label('House:')
+                                            ->options(['owned' => 'Owned', 'rented' => 'Rented', 'w/ parents' => 'W/ parents']),
 
-                                    Forms\Components\Group::make()
-                                            ->columnSpan(4)
+                                    Forms\Components\Fieldset::make("Present Employer")
                                             ->columns(2)
-                                            ->live()
-                                            ->disabled(fn (Forms\Get $get): bool => ! $get('unit_id'))
+                                            ->columnSpan(2)
                                             ->schema([
-                                                    
-                                                    Forms\Components\TextInput::make('unit_engine_number')
-                                                            ->prefix('#')
-                                                            ->columnSpan(1)
-                                                            ->label('Engine Number')
-                                                            ->numeric(),
-
-                                                    Forms\Components\TextInput::make('unit_srp')
-                                                            ->columnSpan(1)
-                                                            ->required(true)
-                                                            ->label('Selling Retail Price:')
-                                                            ->numeric(),
-                                                    Forms\Components\TextInput::make('unit_term')
-                                                            ->columnSpan(1)
-                                                            ->minValue(0)
-                                                            ->required(true)
-                                                            ->label('Term:')
-                                                            ->minValue(1)
-                                                            ->numeric()
-                                                            ->live(500)
-                                                            ->afterStateUpdated(
-                                                                    function(Forms\Get $get, Forms\Set $set){
-                                                                    $_term = $get('unit_term');
-                                                                    $_unit_srp = $get('unit_srp');
-                                                                    if($_term > 1){
-                                                                            $quotient = number_format((float)$_unit_srp/$_term, 2, '.', '');
-                                                                            $set('unit_amort_fin', $quotient);
-                                                                            $set('unit_monthly_amort', $quotient);
-                                                                    }
-                                                                }
-                                                            ),
-                                                    Forms\Components\TextInput::make('unit_ttl_dp')
-                                                                ->required(true)
-                                                                ->label('Total Downpayment:')
-                                                                ->numeric()
-																->minValue(0)
-                                                                ->columnSpan(1)
-                                                                ->live(500)
-                                                                ->afterStateUpdated(
-                                                                        function(Forms\Get $get, Forms\Set $set){
-                                                                                $dp = $get('unit_ttl_dp');
-                                                                                $_term = $get('unit_term');
-                                                                                $_unit_srp = $get('unit_srp');
-                                                                                if($_term > 1 || $get('unit_ttl_dp' <= $get('unit_srp'))){
-                                                                                        $quotient = number_format((float)((float)$_unit_srp - (float)$dp)/$_term, 2, '.', '');
-                                                                                        $set('unit_amort_fin', $quotient);
-                                                                                        $set('monthly_amortization', $quotient);
-                                                                                }
-                                                                        }
-                                                                ),
-                                                    Forms\Components\TextInput::make('unit_monthly_amort')->required(true)
-                                                                ->label('Monthly Amorthization:')
-																->minValue(0)
-                                                                ->numeric(),
-                                                    Forms\Components\Select::make('unit_type')
-                                                                ->required()
-                                                                ->options(['New','Repeat',]),
-                                                    Forms\Components\TextInput::make('unit_amort_fin')
-                                                                ->numeric()
-																->minValue(0)
-                                                                ->required(true)
-                                                                ->label('Amorthization Fin:'),
-                                                    Forms\Components\Select::make('unit_mode_of_payment')
-                                                                ->required(true)
-                                                                ->label('Mode of Payment:')
-                                                                ->options(
-                                                                    ['Office','Field','Bank',]
-                                                                )
-                                                                ->columnSpan(1),
-                                ]),
+                                                    Forms\Components\TextArea::make('applicant_present_business_employer')
+                                                            ->label('Business Employer:')
+                                                            ->columnSpan(2),
+                                                    Forms\Components\TextInput::make('applicant_position')
+                                                            ->label('Position:')
+                                                            ->columnSpan(1),
+                                                    Forms\Components\TextInput::make('applicant_how_long_job_or_business')
+                                                            ->label('How long:')
+                                                            ->columnSpan(1),
+                                            ]),
+    
+                                    Forms\Components\Fieldset::make("Applicant's Business")
+                                            ->columnSpan(1)
+                                            ->columns(1)
+                                            ->schema([
+                                                    Forms\Components\TextArea::make('applicant_business_address')
+                                                            ->label('Address:')
+                                                            ->columnSpan(1),
+                                                    Forms\Components\TextInput::make('applicant_nature_of_business')
+                                                            ->label('Nature of Business:'),
+                                            ]),
+    
+                                    
+                                    Forms\Components\Fieldset::make("Previous Employer")
+                                                                                        ->columns(3)
+                                            ->columnSpan(3)
+                                            ->schema([
+                                                Forms\Components\TextArea::make('applicant_previous_employer')
+                                                        ->label('Employer:')
+                                                        ->columnSpan(1),
+                                                Forms\Components\TextArea::make('applicant_previous_employer_position')
+                                                        ->label('Position:')
+                                                        ->columnSpan(1),
+                                                Forms\Components\TextArea::make('applicant_how_long_prev_job_or_business')
+                                                        ->label('How Long:')
+                                                        ->columnSpan(1),
+                                            ]),
                             ]),
-
-                ])
+        ]),
+    ])
                 ->statePath('data')
                 ->model(CustomerApplication::class);
     }
