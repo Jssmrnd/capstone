1f18f55c2105e71620179b1f771f627dcab3700f
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-09-25 11:28:30 +0800

    changed the starting id to 1M

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,27 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+
+            $table->id()->from(1000000);
+            $table->string("name");
+            $table->string("password");
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string("email");
             $table->rememberToken();
             $table->timestamps();
+
+            // $table->string("first_name");
+            // $table->string("last_name");
+            // $table->string("gender");
+            // $table->date("birthday");
+            // $table->boolean('is_admin')->default(0);
+            // $table->foreignIdFor(Branch::class)
+            // ->nullable()
+            // ->constrained()
+            // ->onUpdate('cascade')
+            // ->nullOnDelete();
+            // $table->string("contact_number");
+
         });
     }
 
