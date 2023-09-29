4ad7238995f331481fcd0ceb2828a88ef3ce7d3a
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-09-29 15:18:02 +0800

    customer works

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,18 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\Authenticatable;
+use App\Models\Scopes\UserScope;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel as FilamentPanel;
+use Filament\Tables\Columns\Layout\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Traits\HasPermissions;
+use Spatie\Permission\Traits\HasRoles;
 
-class Customer extends Model implements Authenticatable
+class Customer extends Authenticatable implements FilamentUser
 {
-    use \Illuminate\Auth\Authenticatable;
-    use HasFactory;
-    use Notifiable;
 
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, HasPermissions;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    public static $filamentUserColumn = 'is_filament_user';
 
     protected $fillable = [
         //persnal information
@@ -21,11 +39,18 @@ class Customer extends Model implements Authenticatable
         "email",
         "password",
     ];
-
     
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function canAccessPanel(FilamentPanel $panel): bool
+    {
+        if(auth()->user()){
+            return true;
+        };
+        return false;
+    }
 
 }
