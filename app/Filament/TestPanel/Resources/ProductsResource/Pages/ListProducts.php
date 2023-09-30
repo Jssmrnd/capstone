a630028f9165a64bcf45cb6ab5b4b46fdce56020
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-09-30 14:44:35 +0800

    created products

---
+++ app/Filament/TestPanel/Resources/ProductsResource/Pages/ListProducts.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\TestPanel\Resources\ProductsResource\Pages;
+
+use App\Filament\TestPanel\Resources\ProductsResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListProducts extends ListRecords
+{
+    protected static string $resource = ProductsResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
