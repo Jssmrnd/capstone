39018406f11eb4ff9f9eb2972d6f33fe1f912e9e
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-10-05 16:45:16 +0800

    added owner for customer applications

--- database/migrations/2023_08_28_032605_create_units_table.php
+++ database/migrations/2023_08_28_032605_create_units_table.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Models\Branch;
+use App\Models\CustomerApplication;
 use App\Models\UnitModel;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -15,19 +17,19 @@ return new class extends Migration
     {
         Schema::create('units', function (Blueprint $table) {
             $table->id();
-
             $table->foreignIdFor(Branch::class);
             $table->foreignIdFor(UnitModel::class);
-
-            $table->string('engine_number')->unique();
-            $table->string('frame_number')->unique();
-            
-            $table->integer('unit_quantity')->default(0);
-            $table->string('status');
-            $table->string('notes')->nullable();
+            $table->string('engine_number')
+                    ->unique();
+            $table->string('frame_number')
+                    ->unique();
+            $table->foreignIdFor(CustomerApplication::class)
+                    ->default(null)->nullable();
+            $table->enum('status',['depo', 'repo', 'brand-new']);
+            $table->string('notes')
+                    ->nullable();
             $table->softDeletes();
             $table->timestamps();
-        
         });
     }
 
@@ -37,6 +39,8 @@ return new class extends Migration
 
     public function down(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::dropIfExists('units');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 };
