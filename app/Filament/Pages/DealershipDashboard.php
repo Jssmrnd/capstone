6905ac2b2abe260d41c6cf8dee4d0ab7201a8e7d
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-10-06 15:30:24 +0800

    added amortization

--- app/Filament/Pages/DealershipDashboard.php
+++ app/Filament/Pages/DealershipDashboard.php
@@ -8,6 +8,7 @@ use Filament\Pages\Page;
 use Filament\Pages\Dashboard as BasePage;
 
 use App\Filament\Widgets\CustomerDues;
+use App\Filament\Widgets\AmortizationRevenueSummary;
 use Filament\Resources\Pages\ManageRecords;
 
 class DealershipDashboard extends BasePage
@@ -25,6 +26,7 @@ class DealershipDashboard extends BasePage
     protected function getHeaderWidgets(): array
     {
         return [
+            AmortizationRevenueSummary::class,
             Widgets\AccountWidget::class,
             CustomerDues::class,
         ];
