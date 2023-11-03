f64b85b3825e6a65e9758076541ef1a910178dc1
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-03 20:06:51 +0800

    commented routes

--- routes/web.php
+++ routes/web.php
@@ -65,15 +65,15 @@ Route::prefix('/products')->group(function () {
     });
 });
 
-Route::get('/login', [CustomerLogin::class, 'index'])
-->name('login');
+// Route::get('/login', [CustomerLogin::class, 'index'])
+// ->name('login');
 
-Route::post('/login', [CustomerLogin::class, 'authenticate']);
+// Route::post('/login', [CustomerLogin::class, 'authenticate']);
 
-Route::get('/register', [CustomerController::class, 'create'])
-->name('register');
+// Route::get('/register', [CustomerController::class, 'create'])
+// ->name('register');
 
-Route::post('/register', [CustomerController::class, 'store']);
+// Route::post('/register', [CustomerController::class, 'store']);
 
-Route::get('/logout', [CustomerController::class, 'destroy'])
-->name('logout');
+// Route::get('/logout', [CustomerController::class, 'destroy'])
+// ->name('logout');
